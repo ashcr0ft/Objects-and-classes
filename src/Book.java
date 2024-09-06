271513803e7f35de074c9faf33fe4dc553cc7bd8
mlstlit <Litvinov.v@mail.ru>
2024-09-06 15:02:40 +0300

    done

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -28,5 +30,27 @@ public class Book {
     // Геттер на автора
     public String getAuthorName() {
         return this.author.getFirstName() + ' ' + this.author.getLastName();
+    }
+
+    @Override
+    public String toString() {
+        return "Книга{" +
+                "Название='" + name + '\'' +
+                ", " + author.toString() +
+                ", Год публикации=" + yearOfPublication +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, yearOfPublication);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
         Book book1 = new Book("Над пропастью во ржи", author1, 1951);
         Author author2 = new Author("Дмитрий", "Глуховский");
         Book book2 = new Book("Метро 2033", author2, 2003);
-        System.out.println(book1.getAuthorName() + " " + book1.getName() + " " + book1.getYearOfPublication());
-        System.out.println(book2.getAuthorName() + " " + book2.getName() + " " + book2.getYearOfPublication());
         book2.setYearOfPublication(2005);
-        System.out.println(book2.getAuthorName() + " " + book2.getName() + " " + book2.getYearOfPublication());
-
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
     }
 }
